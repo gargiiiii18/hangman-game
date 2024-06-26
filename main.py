29dbe097c7828f9c0d27e74ae21415f12b2cf9d7
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2024-06-27 01:06:16 +0530

    file added

---
+++ main.py
@@ -0,0 +1,61 @@
+from replit import clear
+import random
+import hangman_art
+import hangman_words
+
+#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+word_list = hangman_words.word_list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(hangman_art.logo)
+#Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+already_guessed=[]
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    clear()
+    if guess in display:
+        print(f"You have already guessed {guess}")
+    else:   
+
+        #Check guessed letter
+        for position in range(word_length):
+            letter = chosen_word[position]
+            # print(f"Current position: {position}\n Current               letter: {letter}\n Guessed letter: {guess}")
+            if letter == guess:
+                display[position] = letter
+    #Check if user is wrong.
+    
+        if guess not in chosen_word:
+            if guess in already_guessed:
+                print(f"You have already guessed {guess}")
+            else:
+                already_guessed+=guess
+                lives -= 1
+                if lives == 0:
+                    end_of_game = True
+                    print("You lose.")
+                    print(f"The word was: {chosen_word}")
+                else:
+                    print("That letter is not there in the word.You lose one life.")
+        
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_art.stages[lives])
